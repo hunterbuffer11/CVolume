3dcd5a3bc8c72c015c6359f4abf46ad7d0014001
hunter <1667365267@qq.com>
2024-12-04 14:05:53 +0800

    Update method.py

--- pages/method.py
+++ pages/method.py
@@ -142,7 +142,7 @@ elif selected_option == '过滤三角网法':
                 # 保存上传的文件
                 with open(file_path, 'wb') as f:
                     f.write(uploaded_file.read())
-         st.text("提示：该方法处理文件较慢请耐心等待")
+            st.text("提示：该方法处理文件较慢请耐心等待")
             if st.button("处理文件"):
                   st.session_state.processed_volume_delaunay = []
                   with st.spinner('处理中，请稍候...'):
